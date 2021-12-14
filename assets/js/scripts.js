19fa7b3dc17a6a3c1355cfb6f58ab4468ed30c75
Pedro <pedrosbeghen@yahoo.com.br>
2021-12-14 15:44:39 -0300

    Commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -174,3 +174,13 @@ function runLogin() {
     return false;
   }
 }
+
+
+function somenteNumeros(num) {
+  var er = /[^0-9.]/;
+  er.lastIndex = 0;
+  var campo = num;
+  if (er.test(campo.value)) {
+    campo.value = "";
+  }
+}
